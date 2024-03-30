cc83e9bc4d93fded88f53dd546ec5b22530d472d
JinJaeJee <jpleumphiromnat@gmail.com>
2024-03-30 19:31:10 +0700

    feat: login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,75 @@
+import Image from "next/image";
 import React from "react";
 
 const loginPage = () => {
   return (
-    <div className="flex h-screen w-[50%]">
-      <div
-        className="w-full h-full bg-no-repeat bg-cover"
-        style={{ backgroundImage: 'url("coffee-bg-login.jpg")' }}
-      ></div>
-      <div> Form div</div>
+    <div>
+      <section className="min-h-screen flex justify-center items-center bg-green-50">
+        <div className="bg-white flex rounded-2xl shadow-lg max-w-5xl p-5">
+          <div className="w-1/2">
+            <img className="rounded-2xl" src="/coffee-bg-login.jpg" />
+          </div>
+          <div className="flex items-center ju w-1/2">
+            <div className="max-w-md mx-auto">
+              <div className="flex items-start justify-center mb-4">
+                <Image
+                  src="/coffee-logo.png"
+                  alt="Picture of the author"
+                  width={150}
+                  height={150}
+                />
+              </div>
+              <h1 className="flex justify-center">Welcome Back</h1>
+              <p className="text-center text-[12px] my-4"></p>
+
+              <form className="space-y-6">
+                <div>
+                  <label
+                    htmlFor="Username"
+                    className="text-sm font-medium text-gray-700"
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    required
+                    className="w-full p-2 border rounded-md mt-1"
+                  />
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="password"
+                    className="text-sm font-medium text-gray-700"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required
+                    className="w-full p-2 border rounded-md mt-1"
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  className="w-full p-2 bg-green-600 text-white rounded hover:bg-green-700"
+                >
+                  Log In
+                </button>
+              </form>
+
+              <div className="text-center text-sm mt-8">
+                มีปัญหาโปรดติดต่อ เทพอ้วน <span>0929888742</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
