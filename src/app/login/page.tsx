de271697712f76a429ebe21b66e74e205b5d3c53
JinJaeJee <jpleumphiromnat@gmail.com>
2024-03-30 18:22:45 +0700

    feat: setup v1

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const loginPage = () => {
+  return (
+    <div className="flex h-screen w-[50%]">
+      <div
+        className="w-full h-full bg-no-repeat bg-cover"
+        style={{ backgroundImage: 'url("coffee-bg-login.jpg")' }}
+      ></div>
+      <div> Form div</div>
+    </div>
+  );
+};
+
+export default loginPage;
