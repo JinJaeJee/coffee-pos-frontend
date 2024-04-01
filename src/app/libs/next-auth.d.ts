5d691a8597a6f37991efb82893a016fff2dac84b
JinJaeJee <jpleumphiromnat@gmail.com>
2024-04-01 18:07:40 +0700

    feat: nextauth v1

---
+++ src/app/libs/next-auth.d.ts
@@ -0,0 +1,27 @@
+// for next auth
+
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      user_id: number;
+      email: string;
+      name: string;
+    };
+    access_token: string;
+  }
+}
+
+import { JWT } from "next-auth/jwt";
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    account_detail: {
+      user_id: number;
+      email: string;
+      name: string;
+    };
+    access_token: string;
+  }
+}
